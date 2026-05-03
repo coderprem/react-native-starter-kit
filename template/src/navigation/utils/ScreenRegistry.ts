926eb772e87ee57117b2692ab4b8d9adfb0650b8
Prem Kumar <prem.kumar@tothenew.com>
2026-05-03 16:11:08 +0530

    doing anything to make it work

--- template/src/navigation/utils/ScreenRegistry.ts
+++
@@ -1,8 +0,0 @@
-import HomeScreen from '../../features/home/screens/HomeScreen';
-import { ScreenNames } from './ScreenNames';
-import SearchScreen from '../../features/search/screens/SearchScreen';
-
-export const SCREEN_REGISTRY: Record<string, any> = {
-  [ScreenNames.HOME]: HomeScreen,
-  [ScreenNames.SEARCH]: SearchScreen,
-};
--- template/src/storage/storageKeys.ts
+++
@@ -1,5 +0,0 @@
-export const STORAGE_KEYS = {
-  deviceDetails: 'device_details',
-  sideMenu: 'side_menu',
-  navigationBarList: 'navigation_bar_list',
-} as const;
